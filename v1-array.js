0a729e63a53871a96fa688f71114c6e8b8bb6567
Bill Phan <billphan25@gmail.com>
2017-08-21 19:25:23 -0700

    Added array version 1 doc.

---
+++ v1-array.js
@@ -0,0 +1,20 @@
+// To-Do List - Array Version 1.0
+// Application should have a place to store todos.
+// Using variables to store a reference to our array.
+// Syntax is as follows:
+var todos = ['item 1', 'item 2', 'item 3']
+
+// Application should have a way to display todos.
+console.log('My todos:', todos)
+// console.log takes a number of arguments.
+
+// Application should have a way to add new todos.
+todos.push('new todo')
+
+// Application should have a way to change a todo.
+// Changing the first item:
+todos[0] = 'Changed!'
+
+// Application should have a way to delete a todo.
+// Deleting the first item.
+todos.splice(0, 1)
