1e5b590a5a21fe192eac61c7d3c7708cc13a1358
Bill Phan <billphan25@gmail.com>
2017-08-31 18:48:36 -0700

    Finished version 5. Added if/else logic to the program.

--- v5-loops.js
+++ v5-loops.js
@@ -1,10 +1,20 @@
 var todoList = {
   todos: [],
   displayTodos: function() {
-    console.log('My Todos:');
-    // displayTodos should show.todoText
-    for (var i = 0; i < this.todos.length; i++) {
-      console.log(this.todos[i].todoText);
+    // adding logic to the program through if cases.
+    if (this.todos.length === 0) {
+    // triple equals is the most consistent way to compare values.
+      console.log("Your todo list is empty!");
+    } else {
+      console.log('My Todo Items:');
+      // displayTodos should show.completed
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('( )', this.todos[i].todoText);
+        }
+      }
     }
   },
   addTodo: function(todoText) {
